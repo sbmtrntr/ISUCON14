93ec346491400681425440d4b6bba162dcfbedfa
isucon <isucon@example.com>
2024-12-08 02:14:32 +0000

    add index

--- webapp/sql/1-schema.sql
+++ webapp/sql/1-schema.sql
@@ -139,3 +139,4 @@ ALTER TABLE ride_statuses ADD INDEX ride_id_idx(ride_id, created_at DESC);
 ALTER TABLE chair_locations ADD INDEX chair_id_idx(chair_id, created_at DESC);
 ALTER TABLE rides ADD INDEX chair_id_idx(chair_id, updated_at DESC);
 ALTER TABLE chairs ADD INDEX access_token_idx(access_token);
+ALTER TABLE coupons ADD INDEX used_by_idx(used_by);
